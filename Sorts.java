649e86df01419b1840282edf37ccb0de6c8bd737
Hannah-Scheuer <hscheuer20@gmail.com>
2020-12-16 21:21:11 -0500

    I messed up i and j (as per usual) but now it appears to be working

--- Sorts.java
+++ Sorts.java
@@ -9,7 +9,7 @@ public class Sorts{
     for (int i = 0; i<data.length-1;i++){
       swaps = 0;
       for (int j = 0; j<data.length-i-1;j++){
-        if(data[i]>data[i+1]){
+        if(data[j]>data[j+1]){
           temp = data[j];
           data[j] = data[j + 1];
           data[j + 1] = temp;
