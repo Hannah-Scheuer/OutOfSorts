ce7bf0d4fad5e6eb23c0afab8b87bfc94ff9f9ff
Hannah-Scheuer <hscheuer20@gmail.com>
2020-12-21 19:34:23 -0500

    changed the bounds of the second for loop to make sense

--- Sorts.java
+++ Sorts.java
@@ -23,5 +23,31 @@ public class Sorts{
     }
   }
 
+/*
+repeat (numOfElements - 1) times
+  set the first unsorted element as the minimum
+  for each of the unsorted elements
+    if element < currentMinimum
+      set element as new minimum
+  swap minimum with first unsorted position
+*/
+
+  public static void selectionSort(int[] data){
+    int temp = 0;
+    int index = 0;
+    for (int i = 0; i < data.length -1;i++){
+      int small = data[i];
+      for (int j = 0; j< data.length; j++){
+        if (data[j]<small){
+          small = data[j];
+          index = j;
+        }
+      }
+      temp = data[i];
+      data[i] = small;
+      data[index]=temp;
+    }
+  }
+
 
 }
